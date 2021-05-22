8b1d19ffb94884684512af2ca142421000387388
matichewer <schwerdt.matias@gmail.com>
2021-05-22 20:50:46 -0300

    agrego libreria JSON

--- src/main/java/brawlstars/BrawlStars_JavaWrapper/App.java
+++ src/main/java/brawlstars/BrawlStars_JavaWrapper/App.java
@@ -1,13 +1,10 @@
 package brawlstars.BrawlStars_JavaWrapper;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
+
+public class App {
+    public static void main( String[] args ){
+    	
         System.out.println( "Hello World!" );
+        
     }
 }
