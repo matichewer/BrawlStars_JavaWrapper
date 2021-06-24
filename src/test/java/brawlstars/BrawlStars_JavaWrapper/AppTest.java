d2c86d6aae688222d9e42fb058710e343f69791d
matichewer <schwerdt.matias@gmail.com>
2021-06-24 15:33:46 -0300

    Test con JUnit

--- src/test/java/brawlstars/BrawlStars_JavaWrapper/AppTest.java
+++ src/test/java/brawlstars/BrawlStars_JavaWrapper/AppTest.java
@@ -4,6 +4,8 @@ import static org.junit.Assert.assertTrue;
 
 import org.junit.Test;
 
+import GeneratedObjects.Player;
+
 /**
  * Unit test for simple App.
  */
@@ -15,6 +17,11 @@ public class AppTest
     @Test
     public void shouldAnswerWithTrue()
     {
-        assertTrue( true );
+//        assertTrue( true );
+    	
+    	System.out.println("Inicio de JUnit Test\n");
+        Player p1 = new Player("U2VYCYV");
+        assertTrue(p1.getStatus());
+    	System.out.println("Fin de JUnit Test");
     }
 }
